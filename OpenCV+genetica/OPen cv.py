b211138e3eefab58b93a3fa06f39731a44f4ce2d
faceless-alien <78606520+faceless-alien@users.noreply.github.com>
2021-04-20 19:50:53 +0300

    Update OPen cv.py

--- OpenCV+genetica/OPen cv.py
+++ OpenCV+genetica/OPen cv.py
@@ -18,33 +18,27 @@ def Thread_func():
         a.append(Generate_new_img(blank_img))
     images+=a
 def mutate(img):
-    c=[1,-1]
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            random_x=random.randint(0,img.shape[0]-1)
-            random_y=random.randint(0,img.shape[1]-1)
-            
-            rand_int=random.randint(1,3)
-            if rand_int==1:
-                img[random_x][random_y][0]+=random.choice(c)
-            elif rand_int==2:
-                img[random_x][random_y][1]+=random.choice(c)
-            else:
-                img[random_x][random_y][2]+=random.choice(c)
+    for i in range(img.shape[0]//10):
+        for j in range(img.shape[1]//10):
+            img[random.randint(0,img.shape[0]-1)][random.randint(0,img.shape[1]-1)]=numpy.array([numpy.uint8(random.randint(0,255)),numpy.uint8(random.randint(0,255)),numpy.uint8(random.randint(0,255))])
     return img
 def Compare_with_example(image,example):
     perfect_pixels=1
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-            if image[i][j][0]==example[i][j][0] or image[i][j][0]+1==example[i][j][0] or image[i][j][0]-1==example[i][j][0]:
-                if image[i][j][1]==example[i][j][1] or image[i][j][1]+1==example[i][j][1] or image[i][j][1]-1==example[i][j][1]:
-                    if image[i][j][2]==example[i][j][2] or image[i][j][2]+1==example[i][j][2] or image[i][j][2]-1==example[i][j][2]:
+            if image[i][j][0]==example[i][j][0] or image[i][j][0]+2==example[i][j][0] or image[i][j][0]-2==example[i][j][0]:
+                if image[i][j][1]==example[i][j][1] or image[i][j][1]+2==example[i][j][1] or image[i][j][1]-2==example[i][j][1]:
+                    if image[i][j][2]==example[i][j][2] or image[i][j][2]+2==example[i][j][2] or image[i][j][2]-2==example[i][j][2]:
                         perfect_pixels+=1
     if perfect_pixels/(image.shape[0]*image.shape[1])>=0.8:
         return True
     print(str(perfect_pixels)+' идеальных пикселей' )
-threads.append(threading.Thread(target=Thread_func))
+for i in range(5):threads.append(threading.Thread(target=Thread_func))
 threads[0].start()
+threads[1].start()
+threads[2].start()
+threads[3].start()
+threads[4].start()
 while threading.active_count()>1:
     pass
 print('Сгенерировано')
@@ -52,10 +46,12 @@ perfect_image=False
 iterations=0
 while not perfect_image:
     iterations+=1
-    images[0]=mutate(images[0])
+    for i in range(len(images)):
+        images[i]=mutate(images[i])
     img=cv2.imread('bimba.jpg')
-    if Compare_with_example(images[0], img):
-        cv2.imshow('Кек',images[0])
-        cv2.waitKey(0)
-        break
+    for i in images:
+        if Compare_with_example(i, img):
+            cv2.imshow('Кек',i)
+            cv2.waitKey(0)
+            break
     print('Прошла '+str(iterations) +' итерация')
